06dec4ae9330da0786402f643f5e78ae8dda2f1b
Muntasir <alimuntasir2001@gmail.com>
2026-02-11 23:21:19 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB Connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@simple-crud-server.a0arf8b.mongodb.net/?retryWrites=true&w=majority&appName=simple-crud-server`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    
+
+    const db = client.db("lifeShieldDB");
+    const policyCollection = db.collection("policies");
+
+    // Policy Routes
+
+    app.get('/popular-policies', async (req, res) => {
+      const result = await policyCollection.find()
+        .sort({ purchased_count: -1 }) 
+        .limit(6) 
+        .toArray();
+      res.send(result);
+    });
+
+    // 2.Specific Policy's Details 
+    app.get('/policy/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await policyCollection.findOne(query);
+      res.send(result);
+    });
+
+    console.log("Successfully connected to MongoDB!");
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Life Shield Server is running...');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
